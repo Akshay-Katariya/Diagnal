e0d625580e287e6fddc2b35c0e6355f710250c63
Akshay Katariya <akshay.katariya1@gmail.com>
2023-05-30 21:58:03 +0530

    added poster util and common style for text

--- src/components/TextStyles.ts
+++ src/components/TextStyles.ts
@@ -2,6 +2,8 @@ import { TextStyle } from 'react-native'
 import { pxToDp } from '../utils'
 
 export const customTextStyle: TextStyle = {
+  flex: 1,
+  marginTop: pxToDp(24),
   fontSize: pxToDp(36),
   fontFamily: 'titilliumweb_light',
   color: '#ffffff',
---
+++ src/hooks/useDataFetching.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+
+const API_URL = 'https://www.jsonkeeper.com/b/CJI9'
+
+const useDataFetching = () => {
+  const [pageData, setPageData] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(API_URL) // Replace with your actual API endpoint
+        if (!response.ok) {
+          throw new Error('Failed to fetch pageData')
+        }
+        const jsonData = await response.json()
+        setPageData(jsonData)
+      } catch (e) {
+        setError(e.message)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  return { pageData, isLoading, error }
+}
+
+export default useDataFetching
